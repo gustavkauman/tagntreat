45c8d6cf7ae23730b1b607935dfb74eac3f3d8fc
Mads Marquart <madsmtm@gmail.com>
2017-10-10 17:30:48 +0200

    Updated gulpfile and package file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     sass = require('gulp-ruby-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    webserver = require('gulp-webserver');
+    sourcemaps = require('gulp-sourcemaps');//,
+    //webserver = require('gulp-webserver');
 
 gulp.task('js', function() {
-  return gulp.src('resources/js/myscript.js')
+  return gulp.src('./www/public/resources/js/myscript.js')
     .pipe(jshint('./.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('sass', function () {
-    return sass('resources/sass/style.sass', {
+    return sass('./www/public/resources/sass/style.sass', {
       sourcemap: true,
       style: 'compressed'
     })
@@ -19,20 +19,21 @@ gulp.task('sass', function () {
         console.error('Error!', err.message);
     })
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('resources/css'));
+    .pipe(gulp.dest('./www/public/resources/css'));
 });
 
 gulp.task('watch', function() {
   //gulp.watch('resources/js/**/*', ['js']);
-  gulp.watch(['resources/sass/**/*'], ['sass']);
+  gulp.watch(['./www/public/resources/sass/**/*'], ['sass']);
 });
 
+/*
 gulp.task('webserver', function() {
     gulp.src('')
         .pipe(webserver({
             livereload: true,
             open: true
         }));
-});
+});*/
 
 gulp.task('default', ['sass', 'watch']);
