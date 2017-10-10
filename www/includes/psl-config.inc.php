184778b1faf343f1149bbbf6f4d7d227fca85092
Mads Marquart <madsmtm@gmail.com>
2017-10-10 16:24:14 +0200

    Added `psl_config`

---
+++ www/includes/psl-config.inc.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('DATABASE', 'tagntreat');
+define('SECURE', false);
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member")
