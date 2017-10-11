76ece5eccd41c02cc7f876039eed2dd967c368b6
Mads Marquart <madsmtm@gmail.com>
2017-10-11 15:38:39 +0200

    Added example frontpage, now styling is needed

--- www/public/index.php
+++ www/public/index.php
@@ -7,5 +7,67 @@
 </head>
 <body>
 
+	<h1>Traditionsudvalget :: Tag 'n Treat</h1>
+
+
+	<h2>Spillere</h2>
+	<!-- Example data of the people that are still alive -->
+	<div class="holder">
+		<div class="row">
+			Thorfinn Birkelund - 3vz
+		</div>
+		<div class="row">
+			Liam Murphy - 3vz
+		</div>
+	</div>
+
+
+	<h2>Scoreboard</h2>
+	<!-- Example data of the people that are still alive -->
+	<div class="holder">
+		<div class="row">
+			1: Oliver Solow - 3vz, 5 point
+		</div>
+		<div class="row">
+			2: Thorfinn Birkelund - 3vz, 3 point
+		</div>
+		<div class="row">
+			3: Christoffer Larsen - 3vz, 2 point
+		</div>
+		<div class="row">
+			4: Liam Murphy - 3vz, 0 point
+		</div>
+	</div>
+
+
+	<h2>Kills</h2>
+	<!-- Some kind of slideshow here, perhaps showing the various kills? The order here is yet to be determined! -->
+	<div class="holder slideshow">
+		<div class="row">
+			<h3>Oliver Solow 3vz dræber Valiant Fly 3vz (2 point)</h3>
+			<iframe src="">A video</iframe>
+		</div>
+		<div class="row">
+			<h3>Oliver Solow 3vz dræber Amalie Frederiksen 3vz (2 point)</h3>
+			<iframe src="">Another video</iframe>
+		</div>
+		<div class="row">
+			<h3>Oliver Solow 3vz dræber Boline Baastrup 3vz (1 point)</h3>
+			<img src="" alt="A picture" />
+		</div>
+		<div class="row">
+			<h3>Thorfinn Birkelund 3vz dræber Christoffer Larsen 3vz (2 point)</h3>
+			<iframe src="">A third video</iframe>
+		</div>
+		<div class="row">
+			<h3>Christoffer Larsen 3vz dræber Oliver Solow 3vz (2 point)</h3>
+			<iframe src="">A fourth video</iframe>
+		</div>
+		<div class="row">
+			<h3>Christoffer Larsen 3vz dræber Mads Marquart 3vz (1 point)</h3>
+			<img src="" alt="Another picture" />
+		</div>
+	</div>
+
 </body>
 </html>
