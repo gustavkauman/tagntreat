6798105d477551071b8e04ad4b524549e027caf6
Mads Marquart <madsmtm@gmail.com>
2017-10-22 17:17:45 +0200

    Added "game not started" and "game finished" text. Creds to @guk99

--- www/public/index.php
+++ www/public/index.php
@@ -40,29 +40,51 @@ $players_alive = array_filter($players, function($val) {
     <h2 class="thing">Tag and Treat</h2>
 </div>
 <?php if (!is_started()):?>
-    TODO: Something to say if the game is not started yet
+    <div class="row">
+        <p style="text-align:center;color:#fff;">Spillet er endnu ikke startet. Vi glæder os til, at vi kommer igang!</p>
+    </div>
 <?php elseif (count($players_alive) <= 1):?>
-    TODO: Something to say if the game is finished
+    <?php
+    $top_point = reset($players)['points'];
+    $winners = array_filter($players, function($val) {
+        global $top_point;
+        return $val['points'] === $top_point;
+    });
+    ?>
+        <div class="row">
+            <p style="text-align:center;color:#fff;">
+    <?php if (count($winners) === 1): ?>
+            <?php $winner = reset($winners);?>
+            Spillet er slut. Vinderen blev <span style="color:yellow;"><?php echo "{$winner['name']}"; ?></span> fra <span style="color:yellow;"><?php echo "{$winner['classroom']}"; ?></span>. Stort tillykke!
+    <?php else: ?>
+            Spillet er slut. Vinderene blev:<br>
+            <?php foreach ($winners as $winner):?>
+                <span style="color:yellow;"><?php echo "{$winner['name']}"; ?></span> fra <span style="color:yellow;"><?php echo "{$winner['classroom']}"; ?></span><br>
+            <?php endforeach;?>
+            Stort tillykke!
+    <?php endif ?>
+            </p>
+        </div>
 <?php else:?>
 <div class="tables row">
     <div class="col span_1_of_2">
         <table class="top10">
             <tr><td class="table-title"><h2>Top<?php echo (count($players_with_points) >= 10) ? ' 10' : '' ?></h2></td></tr>
-            <?php if (count($players_with_points) > 0):?>
-            <?php foreach ($players_top_10 as $player) {?>
+        <?php if (count($players_with_points) > 0):?>
+            <?php foreach ($players_top_10 as $player):?>
             <tr><td><?php echo $player['name'];?>, <?php echo $player['classroom'];?> - <?php echo $player['points'];?></td></tr>
-            <?php }?>
-            <?php else:?>
+            <?php endforeach;?>
+        <?php else:?>
             <tr><td>Ingen spillere endnu</td></tr>
-            <?php endif;?>
+        <?php endif;?>
         </table>
     </div>
     <div class="col span_1_of_2">
         <table class="players">
             <tr><td class="table-title"><h2>Levende Spillere</h2></td></tr>
-            <?php foreach ($players_alive as $player) {?>
+            <?php foreach ($players_alive as $player):?>
             <tr><td><?php echo $player['name'];?>, <?php echo $player['classroom'];?></td></tr>
-            <?php }?>
+            <?php endforeach;?>
         </table>
     </div>
 </div>
