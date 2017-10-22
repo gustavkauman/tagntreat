0b5254c26f0fdc0a7d9f224c6c85806582ef8161
guk99 <gustav.kauman@gmail.com>
2017-10-22 21:11:08 +0200

    Fix missing spaces + footer added
    Gefion Programming logo missing in footer though

--- www/public/index.php
+++ www/public/index.php
@@ -31,7 +31,7 @@ $players_alive = array_filter($players, function($val) {
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="/resources/css/style.css">
     <title>Tag 'n Treat :: Traditionsudvalget</title>
-    <?php if (isset($refresh)): ?>
+    <?php if (isset($refresh)) : ?>
         <script type="text/javascript">setTimeout(function(){document.location = '/'}, 1000*<?php echo $refresh; ?>);</script>
     <?php endif ?>
 </head>
@@ -42,11 +42,11 @@ $players_alive = array_filter($players, function($val) {
 <div class="row">
     <h2 class="thing">Tag and Treat</h2>
 </div>
-<?php if (!is_started()):?>
+<?php if (!is_started()) :?>
     <div class="row">
         <p style="text-align:center;color:#fff;">Spillet er endnu ikke startet. Vi glæder os til, at vi kommer igang!</p>
     </div>
-<?php elseif (count($players_alive) <= 1):?>
+<?php elseif (count($players_alive) <= 1) :?>
     <?php
     $top_point = reset($players)['points'];
     $winners = array_filter($players, function($val) {
@@ -56,28 +56,28 @@ $players_alive = array_filter($players, function($val) {
     ?>
         <div class="row">
             <p style="text-align:center;color:#fff;">
-    <?php if (count($winners) === 1): ?>
+    <?php if (count($winners) === 1) : ?>
             <?php $winner = reset($winners);?>
             Spillet er slut. Vinderen blev <span style="color:yellow;"><?php echo "{$winner['name']}"; ?></span> fra <span style="color:yellow;"><?php echo "{$winner['classroom']}"; ?></span>. Stort tillykke!
-    <?php else: ?>
+    <?php else : ?>
             Spillet er slut. Vinderene blev:<br>
-            <?php foreach ($winners as $winner):?>
+            <?php foreach ($winners as $winner) :?>
                 <span style="color:yellow;"><?php echo "{$winner['name']}"; ?></span> fra <span style="color:yellow;"><?php echo "{$winner['classroom']}"; ?></span><br>
             <?php endforeach;?>
             Stort tillykke!
     <?php endif ?>
             </p>
         </div>
-<?php else:?>
+<?php else :?>
 <div class="tables row">
     <div class="col span_1_of_2">
         <table class="top10">
             <tr><td class="table-title"><h2>Top<?php echo (count($players_with_points) >= 10) ? ' 10' : '' ?></h2></td></tr>
-        <?php if (count($players_with_points) > 0):?>
-            <?php foreach ($players_top_10 as $player):?>
+        <?php if (count($players_with_points) > 0) :?>
+            <?php foreach ($players_top_10 as $player) :?>
             <tr><td><?php echo $player['name'];?>, <?php echo $player['classroom'];?> - <?php echo $player['points'];?></td></tr>
             <?php endforeach;?>
-        <?php else:?>
+        <?php else :?>
             <tr><td>Ingen spillere endnu</td></tr>
         <?php endif;?>
         </table>
@@ -85,7 +85,7 @@ $players_alive = array_filter($players, function($val) {
     <div class="col span_1_of_2">
         <table class="players">
             <tr><td class="table-title"><h2>Levende Spillere</h2></td></tr>
-            <?php foreach ($players_alive as $player):?>
+            <?php foreach ($players_alive as $player) :?>
             <tr><td><?php echo $player['name'];?>, <?php echo $player['classroom'];?></td></tr>
             <?php endforeach;?>
         </table>
